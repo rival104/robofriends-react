fa1afbaf0bcb77205baf355f3f7693a3e10a12ba
rival104 <navid.rvl@gmail.com>
2019-12-16 12:08:40 -0500

    Implemented Search functionality and added style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
+import './App.css';
 
-const App = () => {
-  return (
-    <Fragment>
-      <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />
-      </div>
-    </Fragment>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    };
+  }
+  //experimental. alternative is bind it in constructor.
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value});
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+
+    return (
+      <Fragment>
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
