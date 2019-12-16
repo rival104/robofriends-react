af09800c50ef22622146f9552bf56e59bcda00d5
rival104 <navid.rvl@gmail.com>
2019-12-16 07:46:46 -0500

    Modified Hello component

--- src/Hello.js
+++ src/Hello.js
@@ -5,7 +5,7 @@ class Hello extends Component {
   render() {
     return (
       <div className="f1 tc">
-        <h1>Hello</h1>
+        <h1>Hi!</h1>
         <p>Welcome to my React World!</p>
       </div>
     );
