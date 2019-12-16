6b8a61a7eeb2e28c0cf1f82ced85119a2671c01c
rival104 <navid.rvl@gmail.com>
2019-12-16 10:03:41 -0500

    Created the robots in view

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. p
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => {
+  return (
+    <Fragment>
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
       </div>
-    );
-  }
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = ({name, id, email}) => {
+  //{name, id, email} = props;
+  return (
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+export default Card;
