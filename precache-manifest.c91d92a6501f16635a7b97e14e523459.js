cbbe9b61610b3d1798e458f39ff3b0bb6b4f6fb7
rival104 <navid.rvl@gmail.com>
2019-12-17 10:32:19 -0500

    Updates

---
+++ precache-manifest.c91d92a6501f16635a7b97e14e523459.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fca93a4c0d8c5e418a56303162a3fc2",
+    "url": "/robofriends-react/index.html"
+  },
+  {
+    "revision": "7eb3beebc5013af825e8",
+    "url": "/robofriends-react/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f62db8d48f925c60c6da",
+    "url": "/robofriends-react/static/css/main.69f8eb12.chunk.css"
+  },
+  {
+    "revision": "7eb3beebc5013af825e8",
+    "url": "/robofriends-react/static/js/2.908ef5fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends-react/static/js/2.908ef5fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f62db8d48f925c60c6da",
+    "url": "/robofriends-react/static/js/main.99c8cb96.chunk.js"
+  },
+  {
+    "revision": "95db1f19301754e275bb",
+    "url": "/robofriends-react/static/js/runtime-main.bf2dacff.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
